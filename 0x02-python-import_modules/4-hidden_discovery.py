22e4000280d908733c68904a2dabd1c883b0d084
Kingsley Fynn Asaam <kingsley.asaam@gmail.com>
2023-06-08 20:06:22 -0700

    fixed the indentation issues

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-     """Print all the module names defined by hidden_4"""
+    """Print all the module names defined by hidden_4"""
     import hidden_4
 
     name = dir(hidden_4)
