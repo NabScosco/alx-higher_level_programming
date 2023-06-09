c79bd60967b6f08074494a2eeff1bd137b4ea975
Kingsley Fynn Asaam <kingsley.asaam@gmail.com>
2023-06-08 19:13:49 -0700

    Did all the tasks together

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+if __name__ == "_main_":
+
+    from sys import argv
+
+    num_args = len(argv) - 1
+
+    print(f"{num_args} {'argument' if num_args == 1 else 'arguments'}{'.' if num_args == 0 else ':'}")
+
+    for i in range(1, len(argv)):
+    print(f"{i}: {argv[i]}")
+
