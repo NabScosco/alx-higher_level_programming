f7deeab9ffaeba13d9d9147bc1b4394c774d0f5c
Kingsley Fynn Asaam <kingsley.asaam@gmail.com>
2023-06-11 10:21:39 -0700

    removed the 1 from the def

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """Secure access to an element in a list"""
-1def element_at(my_list, idx):
-    if idx < 0 or idx > (len(my_list)-1):
+def element_at(my_list, idx):
+    if idx < 0 or idx >= (len(my_list)):
         return None
     return (my_list[idx])
