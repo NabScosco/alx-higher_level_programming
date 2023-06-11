d4e58ae65b478786f121a837b522cb24433654b2
Kingsley Fynn Asaam <kingsley.asaam@gmail.com>
2023-06-11 09:01:03 -0700

    added a prnt statement to the return value

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,4 +3,4 @@
 1def element_at(my_list, idx):
     if idx < 0 or idx >= len(my_list):
         return None
-    return my_list[idx]
+    return print("Eliment at index {:d} is {}".format(my_list[idx]))
