99be476482aaa4f19862fb24a3020887d33334d9
Kingsley Fynn Asaam <kingsley.asaam@gmail.com>
2023-06-11 15:17:45 -0700

    Task 2 and 3

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -7,4 +7,5 @@ def replace_in_list(my_list, idx, element):
             return my_list
         if idx >= len(my_list):
             return my_list
-        return (my_list[idx]=element)
+        my_list[idx] = element
+        return my_list
