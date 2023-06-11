91d8e7fe481454f5de150eba47d4a0d8f5f00263
Kingsley Fynn Asaam <kingsley.asaam@gmail.com>
2023-06-11 10:13:11 -0700

    added comment

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,6 +2,6 @@
 
 """Secure access to an element in a list"""
 1def element_at(my_list, idx):
-    if idx < 0 or idx >= len(my_list):
+    if idx < 0 or idx > (len(my_list)-1):
         return None
     return (my_list[idx])
