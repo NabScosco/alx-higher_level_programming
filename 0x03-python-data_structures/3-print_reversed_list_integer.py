ad520b4df08cdbffc07ffed9e398783b92c412c4
Kingsley Fynn Asaam <kingsley.asaam@gmail.com>
2023-06-11 15:45:27 -0700

    removed comment and trailling white spacees

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,8 +2,7 @@
 
 
 def print_reversed_list_integer(my_list=[]):
-
     if isinstance(my_list, list):
         my_list.reverse()
-    for i in my_list:
-        print("{:d}".format(i))
+    for element in my_list:
+        print("{:d}".format(element))
