f6d5afce1df677335b02d9080495c3abd5fd283f
Kingsley Fynn Asaam <kingsley.asaam@gmail.com>
2023-06-14 21:34:04 -0700

    Tasks 1 to advanced

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def number_keys(a_dictionary):
+    count = 0
+
+    # Iterate over the keys in the dictionary
+    for key in a_dictionary:
+        count += 1
+
+    return count
