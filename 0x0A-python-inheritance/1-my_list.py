7823e253930a302747372b0d949cb619e7414361
Kingsley <79924477+NabScosco@users.noreply.github.com>
2023-08-30 19:15:25 +0000

    Delete 0x0A-python-inheritance directory

--- 0x0A-python-inheritance/1-my_list.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""This is a  function that defines an inherited list"""
-
-
-class MyList(list):
-    """Returns a sorted list"""
-
-    def print_sorted(self):
-        print(sorted(self))
--- 0x0A-python-inheritance/6-base_geometry.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Defines a base geometry class BaseGeometry."""
-
-
-class BaseGeometry:
-    """This is the base geometry"""
-    def area(self):
-        """Has not been implimented"""
-        raise Exception("area() is not implemented")
