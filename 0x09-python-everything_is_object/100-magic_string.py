b11642be8be81b88454972613beb04e060c3c915
Kingsley Fynn Asaam <kingsley.asaam@gmail.com>
2023-09-06 17:26:08 -0700

    updated 100-magic_string.py

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,6 +1,3 @@
-#!/usr/bin/python3
-
-
 def magic_string():
-    magic_string.count = getattr(magic_string, 'count', 0) + 1
-    return "BestSchool" + (", BestSchool" * (magic_string.count - 1))
+    magic_string.counter = getattr(magic_string, 'counter', 0) + 1
+    return "BestSchool, " * magic_string.counter + "BestSchool"
