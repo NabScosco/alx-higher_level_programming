afa1173c67ec86d15b026c378dd66664f3d222ed
Kingsley Fynn Asaam <kingsley.asaam@gmail.com>
2023-09-06 17:12:36 -0700

    updated the code in 19-copy_list.py

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+copy_list = __import__('19-copy_list').copy_list
 
+my_list = [1, 2, 3]
+print(my_list)
 
-def copy_list(ll):
-    return ll.copy()
+new_list = copy_list(my_list)
+
+print(my_list)
+print(new_list)
+
+print(new_list == my_list)
+print(new_list is my_list)
