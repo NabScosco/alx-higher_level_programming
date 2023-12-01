3f28cab9d543a1a0f7b1daf29128add1c8b51314
Kingsley Fynn Asaam <kingsley.asaam@gmail.com>
2023-11-30 21:41:29 -0800

    deleted the new lines

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -2,4 +2,3 @@
 # This script takes in a URL as an argument, sends a GET request to the URL,
 # and displays the body of the response with a custom header X-School-User-Id
 curl -sH "X-School-User-Id: 98" "$1"
-
