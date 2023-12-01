4232f3a0cc079f66fcd6a513b041723b59e664f5
Kingsley Fynn Asaam <kingsley.asaam@gmail.com>
2023-12-01 14:33:46 -0800

    Wrote a Python script that fetches https://alx-intranet.hbtn.io/status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+    Fetches https://alx-intranet.hbtn.io/status
+"""
+
+import urllib.request
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        content = response.read()
+        utf8_content = content.decode('utf-8')
+
+        print("Body response:")
+        print(f"\t- type: {type(content)}")
+        print(f"\t- content: {content}")
+        print(f"\t- utf8 content: {utf8_content}")
