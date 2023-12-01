a3933881d14afdd09976975e8316198ceca69509
Kingsley Fynn Asaam <kingsley.asaam@gmail.com>
2023-11-30 21:58:56 -0800

    fixed file 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# This script takes in a URL, sends a POST request to the passed URL,
-# and displays the body of the response with specified parameters
-curl -sX POST "$1" -d "email=test@gmail.com" -d "subject=I will always be here for PLD"
+# Sends a POST request to the URL with specified parameters and displays the body of the response
+curl -sX POST "$1" -d "email=test@gmail.com&subject=I will always be here for PLD"
